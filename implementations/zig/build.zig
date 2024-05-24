3ece035acff2c9aa960be7235e5c44497d22926b
Frank Denis <github@pureftpd.org>
2024-05-25 00:49:23 +0200

    Fix for Zig 0.13

--- implementations/zig/build.zig
+++ implementations/zig/build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const lib_aegis128x = b.addStaticLibrary(.{
         .name = "aegis128x",
-        .root_source_file = .{ .path = "aegis128x.zig" },
+        .root_source_file = b.path("aegis128x.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -14,14 +14,14 @@ pub fn build(b: *std.Build) void {
 
     const lib_aegis256x = b.addStaticLibrary(.{
         .name = "aegis256x",
-        .root_source_file = .{ .path = "aegis256x.zig" },
+        .root_source_file = b.path("aegis256x.zig"),
         .target = target,
         .optimize = optimize,
     });
     b.installArtifact(lib_aegis256x);
 
     const test_vectors = b.addTest(.{
-        .root_source_file = .{ .path = "test.zig" },
+        .root_source_file = b.path("test.zig"),
         .target = target,
         .optimize = optimize,
     });
